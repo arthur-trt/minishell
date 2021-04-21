3117828e6adcfdb477da5133f729c5f2830c4552
arthur-trt <atrouill@student.42.fr>
2021-04-21 17:17:40 +0200

    test

--- srcs/input/char.c
+++ srcs/input/char.c
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 19:31:34 by atrouill          #+#    #+#             */
-/*   Updated: 2021/04/21 17:16:59 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:17:37 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ bool	insert_char(t_line *input, int c)
 	return (true);
 }
 
-bool	delete_char(t_line *input, int c)
-{
-	if (input->cursor == 0)
-		return (false);
+//bool	delete_char(t_line *input, int c)
+//{
+//	if (input->cursor == 0)
+//		return (false);
 
-}
+//}
