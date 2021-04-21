2ac14a7f04c6e94930c3e8dd67ef7924ca517446
arthur-trt <atrouill@student.42.fr>
2021-04-21 16:35:05 +0200

    Test

--- srcs/input/char.c
+++ srcs/input/char.c
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 19:31:34 by atrouill          #+#    #+#             */
-/*   Updated: 2021/04/21 15:49:02 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/04/21 16:13:45 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ bool	insert_char(t_line *input, int c)
 {
 	if (input->lenght + 1 >= MAX_CMD_LINE)
 		return (false);
-	memmove(input->line + input->cursor + 1, input->line + input->cursor,
+	memmove(input->line + input->cursor, input->line + input->cursor - 1,
 		input->lenght - input->cursor + 1);
 	input->line[input->cursor] = c;
 	input->lenght++;
--- srcs/input/move_cursor.c
+++ srcs/input/move_cursor.c
@@ -6,7 +6,7 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 18:47:31 by atrouill          #+#    #+#             */
-/*   Updated: 2021/04/20 20:13:26 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/04/21 16:34:34 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ void	move_cursor_left(t_line *input)
 	input->cursor--;
 //	printf("cursor_pos:\n\tcol : %zu\nrow : %zu\n", input->cursor_pos.col, input->cursor_pos.row);
 //	printf("win_size:\n\tcol : %zu\nrow : %zu\n", input->win_size.col, input->win_size.row);
-	pos.col = (input->cursor_pos.col + input->cursor) % input->win_size.col;
-	pos.row = input->cursor_pos.row; // + input->cursor) / input->win_size.row;
-	tputs(tgoto(tgetstr("cm", NULL), pos.col - 1, pos.row - 1), 1, &outfun);
+	pos.col = ((input->cursor_pos.col + input->cursor) %
+		input->win_size.col) - 1;
+	pos.row = input->cursor_pos.row - 1; // + input->cursor) / input->win_size.row;
+	tputs(tgoto(tgetstr("cm", NULL), pos.col, pos.row), 1, &outfun);
 }
 
 void	move_cursos_right(t_line *input)
@@ -35,7 +36,8 @@ void	move_cursos_right(t_line *input)
 	input->cursor++;
 //	printf("cursor_pos:\n\tcol : %zu\nrow : %zu\n", input->cursor_pos.col, input->cursor_pos.row);
 //	printf("win_size:\n\tcol : %zu\nrow : %zu\n", input->win_size.col, input->win_size.row);
-	pos.col = (input->cursor_pos.col + input->cursor) % input->win_size.col;
-	pos.row = input->cursor_pos.row; // + input->cursor) / input->win_size.row;
-	tputs(tgoto(tgetstr("cm", NULL), pos.col - 1, pos.row - 1), 1, &outfun);
+	pos.col = ((input->cursor_pos.col + input->cursor) %
+		input->win_size.col) - 1;
+	pos.row = input->cursor_pos.row - 1; // + input->cursor) / input->win_size.row;
+	tputs(tgoto(tgetstr("cm", NULL), pos.col, pos.row ), 1, &outfun);
 }
