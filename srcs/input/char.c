b1ade951285701f07657a5f73e5746e97980a19d
arthur-trt <atrouill@student.42.fr>
2021-04-21 15:49:12 +0200

    test

--- srcs/input/char.c
+++ srcs/input/char.c
@@ -6,17 +6,18 @@
 /*   By: atrouill <atrouill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 19:31:34 by atrouill          #+#    #+#             */
-/*   Updated: 2021/04/21 15:44:38 by atrouill         ###   ########.fr       */
+/*   Updated: 2021/04/21 15:49:02 by atrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <string.h>
 
 bool	insert_char(t_line *input, int c)
 {
 	if (input->lenght + 1 >= MAX_CMD_LINE)
 		return (false);
-	ft_memmove(input->line + input->cursor, input->line + input->cursor - 1,
+	memmove(input->line + input->cursor + 1, input->line + input->cursor,
 		input->lenght - input->cursor + 1);
 	input->line[input->cursor] = c;
 	input->lenght++;
